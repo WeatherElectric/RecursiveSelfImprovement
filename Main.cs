390a79abc7c730e77217c05a4015ce8fc9bb1087
Mabel <mae@weatherelectric.xyz>
2024-08-02 00:11:55 -0700

    Add download link

--- Main.cs
+++ Main.cs
@@ -12,7 +12,7 @@ public class Main : MelonMod
     internal const string Author = "FragileDeviations";
     internal const string Company = "Weather Electric";
     internal const string Version = "1.0.0";
-    internal const string DownloadLink = null;
+    internal const string DownloadLink = "https://thunderstore.io/c/bonelab/p/SoulWithMae/RecursiveSelfImprovement/";
     
     internal static Page MenuCat { get; private set; }
     internal static Assembly CurrAsm { get; } = Assembly.GetExecutingAssembly();
